8adebf3163130db652ac1e577e40ccdbc6ff8748
SwapnilKakade <swapnilkakade@gmail.com>
2023-08-16 15:59:17 +0530

    correct loginservice

--- gaminggears/gaminggears/src/main/java/com/demo/gaminggears/service/LoginService.java
+++ gaminggears/gaminggears/src/main/java/com/demo/gaminggears/service/LoginService.java
@@ -2,12 +2,18 @@ package com.demo.gaminggears.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.demo.gaminggears.entity.Distributor;
 import com.demo.gaminggears.entity.Login;
 import com.demo.gaminggears.repository.DistributorRepository;
 
+@Service
 public class LoginService implements ILoginService{
+	@Autowired
 	DistributorRepository distributorRepository;
+	
 	@Override
 	public List<Distributor> getAllDistributors() {
 		List<Distributor> dislist =distributorRepository.findAll();
