633e460a781d08ca7dfc75dfe444172a8c7c51de
Gajendrakumar B <bhokregajendra@gmail.com>
2023-08-23 18:35:56 +0530

    Removed Area, city etc

--- gaminggears/gaminggears/src/main/java/com/demo/gaminggears/entity/Customer.java
+++ gaminggears/gaminggears/src/main/java/com/demo/gaminggears/entity/Customer.java
@@ -1,14 +1,11 @@
 package com.demo.gaminggears.entity;
 
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
