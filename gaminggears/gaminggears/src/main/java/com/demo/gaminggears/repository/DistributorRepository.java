f2e2fcc37ea7f92a8e8043d92fe3c49f44016c23
Gajendrakumar B <bhokregajendra@gmail.com>
2023-08-27 20:07:16 +0530

    corrected again dis sales class

--- gaminggears/gaminggears/src/main/java/com/demo/gaminggears/repository/DistributorRepository.java
+++ gaminggears/gaminggears/src/main/java/com/demo/gaminggears/repository/DistributorRepository.java
@@ -3,6 +3,7 @@ package com.demo.gaminggears.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.demo.gaminggears.entity.Distributor;
@@ -16,13 +17,8 @@ import java.util.List;
 		@Query(value = "SELECT * FROM distributor WHERE email = ?1", nativeQuery = true)
 		List<Distributor> getDistributorByEmail(String email);
 		
-		@Query(value = "SELECT o.proid as proid, p.proname as proname, count(o.proid) as qtysold, SUM(o.price) AS sales " +
-		           "FROM Orders o " +
-		           "JOIN Product p ON o.proid = p.proid " +
-		           "JOIN Distributor d ON p.disid = d.disid " +
-		           "WHERE d.disid = ?1 " +
-		           "GROUP BY o.proid", nativeQuery = true,name = "Mapp")
-		List<SalesStatisticsDTO> findSalesStatisticsByDistributorId(int distributorId);
+		
+		List<SalesStatisticsDTO> findSalesStatisticsByDistributorId(@Param("disid") int distributorId);
 
 
 	    
